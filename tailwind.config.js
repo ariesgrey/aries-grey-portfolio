17d0eaa8e9dd2a87c09e8b253235dc021ee8cc2a
aries grey <ariesgrey88@gmail.com>
2024-03-26 21:20:13 -0500

    build: set up basic structure, styles, and known dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,55 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		screens: {
+			xs: "360px",
+			sm: "480px",
+			...defaultTheme.screens,
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: colors.black,
+			white: colors.white,
+			teal: {
+				100: "#d0dfdf",
+				200: "#b8cfcf",
+				light: "#a0bfbf",
+				400: "#84acac",
+				500: "#679898",
+				600: "#4b8585",
+				700: "#2e7171",
+				DEFAULT: "#125e5e",
+				900: "#0e4b4b",
+				950: "#092f2f",
+			},
+			gray: {
+				50: "#f5f5f6",
+				100: "#e6e6e7",
+				200: "#cfd0d2",
+				light: "#aeafb2",
+				400: "#85868b",
+				500: "#6a6b70",
+				600: "#5a5b60",
+				700: "#4d4d51",
+				800: "#444546",
+				dark: "#323234",
+				950: "#252527",
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-montserrat)", ...defaultTheme.fontFamily.sans],
+				mono: ["var(--font-inconsolata)", ...defaultTheme.fontFamily.mono],
+			},
+		},
+	},
+	plugins: [],
 };
